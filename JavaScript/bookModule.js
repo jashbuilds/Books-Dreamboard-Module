6541a0b12f0212cf15c7ac8da43431a09c419bec
Jash Vadgama <jashvadgama.ownai@gmail.com>
2026-04-16 18:40:36 +0530

    [WIP] Preview PDF

--- JavaScript/bookModule.js
+++ JavaScript/bookModule.js
@@ -15,29 +15,17 @@ db.version(1).stores({
   books: "++id, name, category, file",
 });
 
-// Function to generate Random book IDs.
-const randomId = () => {
-  return Math.floor(Math.random() * 1000);
-};
-
-// const getBooksInfo = async () => {
-//   // const books = await db.books.toArray()
-//   currentDisplayBooks = await db.books.toArray();
-
-// };
-// getBooksInfo().then(() => renderBooks());
-
+let books;
 const renderBooks = async (category = "All") => {
-  let books;
 
   if (category === "All") {
     books = await db.books.toArray();
   } else {
     books = await db.books.where("category").equals(category).toArray();
   }
-
+  
   if (books.length === 0) {
-    booksGrid.innerHTML = '<p class="fw-medium d-flex h4 justify-content-center align-items-center mx-auto">No books found!</p>';
+    booksGrid.innerHTML = "<p class='fw-medium d-flex h4 justify-content-center align-items-center mx-auto'>No books found!</p>";
     return;
   }
 
@@ -45,11 +33,11 @@ const renderBooks = async (category = "All") => {
     .map(
       (book) =>
                             `<div class="col-xl-2 col-lg-3 col-sm-4 col-6">
-                                <div onclick="location.href='../HTML/bookPreview.html';"
+                                <div 
                                     class="card booksCard shadow cursor-pointer border-5 border-primary border-start border-bottom-0 border-top-0 border-end-0 rounded-3">
-                                    <div class="card-body">
+                                    <div class="card-header bg-transparent border-bottom-0 align-content-center" onclick="openBookFromId('${book.id}')">
                                         <div
-                                            class="d-flex mb-2 justify-content-between align-items-center">
+                                            class="d-flex py-1 justify-content-between align-items-center">
                                             <p
                                                 class="card-title text-wrap h5 w-100 m-0 fw-semibold overflow-hidden ${book.category === "Self-help" ? "text-primary" : book.category === "Finance" ? "text-success" : book.category === "Fiction" ? "text-danger" : book.category === "Historical" ? "text-secondary" : book.category === "Sci-Fi" ? "text-warning" : ""} ">${book.category}</p>
                                             <div class="dropdown">
@@ -82,6 +70,8 @@ const renderBooks = async (category = "All") => {
                                             </div>
 
                                         </div>
+                                    </div>
+                                    <div class="card-body px-3 py-0" onclick="location.href='../HTML/bookPreview.html';">
                                         <p class="fw-medium fs-5">${book.name}</p>
                                     </div>
                                     <div class="card-footer infoBtn border-top-0 bg-transparent py-2 px-3 justify-content-end align-items-center">
@@ -105,7 +95,7 @@ const filterBooks = () => {
   renderBooks(selectedCategory);
 };
 
-addBookForm.addEventListener("submit", async (e) => {
+addBookForm?.addEventListener("submit", async (e) => {
   e.preventDefault();
   await db.books.add({
     name: bookName.value,
@@ -120,7 +110,7 @@ addBookForm.addEventListener("submit", async (e) => {
   renderBooks();
 });
 
-addBookModal.addEventListener("hidden.bs.modal", () => {
+addBookModal?.addEventListener("hidden.bs.modal", () => {
   bookName.value = "";
   bookCategory.value = "All";
   bookPDF.value = "";
@@ -139,5 +129,44 @@ const validateFormInput = () => {
   submitBtn.disabled = !isFormValid;
 };
 
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
 
-export default renderBooks;
+const openBookFromId = async (id) => {
+  const book = await db.books.get(id);
+  console.log(book);
+
+  const file = book.file;
+
+  const arrayBuffer = await file.arrayBuffer();
+
+  loadPDF(arrayBuffer);
+};
+
+
+let pdfDoc = null;
+let currentPage = 1;
+
+async function loadPDF(data) {
+  pdfDoc = await pdfjsLib.getDocument({ data }).promise;
+  currentPage = 1;
+
+  renderPage(currentPage);
+}
+
+async function renderPage(pageNum) {
+  const page = await pdfDoc.getPage(pageNum);
+
+  const canvas = document.getElementById("pdfCanvas");
+  const ctx = canvas.getContext("2d");
+
+  const viewport = page.getViewport({ scale: 1.5 });
+
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+  await page.render({
+    canvasContext: ctx,
+    viewport: viewport,
+  }).promise;
+}
--- JavaScript/bookPreview.js
+++
@@ -1,5 +0,0 @@
-import renderBooks from "../JavaScript/bookModule.js"
-
-const bookName = document.getElementById("bookName")
-
-console.log(bookName);
